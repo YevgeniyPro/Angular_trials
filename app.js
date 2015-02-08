fcfd2fe228f65d6261b8ef51927b6e75e7e0f360
yevg <e.protsyuk@gmail.com>
2015-02-08 18:09:49 +0100

    basic built-in directives

--- app.js
+++ app.js
@@ -2,6 +2,31 @@
 
 	var app = angular.module('store', []); 
 
-	app.controller(StoreController)
+	app.controller('StoreController', function(){
+		this.products = gems;
 
-})();d
+	});
+
+
+	var gems = [ 
+	{
+
+		name: 'Dodecahedron',
+		price : 2.95,
+		description : 'description of Dodecaheron',
+		canPurchase : true, 
+		soldOut : true
+
+	},  
+	
+	{
+		name: 'Azurit',
+		price : 3.95,
+		description : 'description of Azurit',
+		canPurchase : true, 
+		soldOut : false
+	}
+	] ;
+
+	
+})();
